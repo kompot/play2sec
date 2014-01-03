404cf55c7cdcded4ac9b7e03cfb4f82206de85dd
Anton Fedchenko <kompotik@gmail.com>
2014-01-03 23:35:32 +0400

    Move all sensitive data to a separate file so it's not committed accidentally.

---
+++ test/SensitiveConfigurationData.scala
@@ -0,0 +1,21 @@
+object SensitiveConfigurationData {
+  // fill all the values for tests to work
+  // should NOT be committed to VCS with non empty values
+  val config = Map(
+    "test.facebook.login" -> "",
+    "test.facebook.password" -> "",
+    "test.facebook.id" -> "",
+    "play2sec.facebook.clientId" -> "",
+    "play2sec.facebook.clientSecret" -> "",
+    "test.twitter.login" -> "",
+    "test.twitter.password" -> "",
+    "test.twitter.id" -> "",
+    "play2sec.twitter.consumerKey" -> "",
+    "play2sec.twitter.consumerSecret" -> "",
+    "test.google.login" -> "",
+    "test.google.password" -> "",
+    "test.google.id" -> "",
+    "play2sec.google.clientId" -> "",
+    "play2sec.google.clientSecret" -> ""
+  )
+}
