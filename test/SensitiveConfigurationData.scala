0a71383f5af80d518d1ba33d6fee5c0218c738c2
Anton Fedchenko <kompotik@gmail.com>
2014-01-04 12:47:19 +0400

    Refactor.

--- test/SensitiveConfigurationData.scala
+++ test/SensitiveConfigurationData.scala
@@ -1,21 +1,39 @@
+import com.github.kompot.play2sec.authentication.providers.oauth1.twitter
+.TwitterAuthProvider
+import com.github.kompot.play2sec.authentication.providers.oauth2.facebook
+.FacebookAuthProvider
+import com.github.kompot.play2sec.authentication.providers.oauth2.google
+.GoogleAuthProvider
+
 object SensitiveConfigurationData {
+  val configByProvider: Map[String, Map[String, String]] = Map(
+    FacebookAuthProvider.PROVIDER_KEY -> Map(
+      "test.facebook.login" -> "",
+      "test.facebook.password" -> "",
+      "test.facebook.id" -> "",
+      "play2sec.facebook.clientId" -> "",
+      "play2sec.facebook.clientSecret" -> ""
+    ),
+    TwitterAuthProvider.PROVIDER_KEY -> Map(
+      "test.twitter.login" -> "",
+      "test.twitter.password" -> "",
+      "test.twitter.id" -> "",
+      "play2sec.twitter.consumerKey" -> "",
+      "play2sec.twitter.consumerSecret" -> ""
+    ),
+    GoogleAuthProvider.PROVIDER_KEY -> Map(
+      "test.google.login" -> "",
+      "test.google.password" -> "",
+      "test.google.id" -> "",
+      "play2sec.google.clientId" -> "",
+      "play2sec.google.clientSecret" -> ""
+    )
+  )
+
   // fill all the values for tests to work
   // should NOT be committed to VCS with non empty values
-  val config = Map(
-    "test.facebook.login" -> "",
-    "test.facebook.password" -> "",
-    "test.facebook.id" -> "",
-    "play2sec.facebook.clientId" -> "",
-    "play2sec.facebook.clientSecret" -> "",
-    "test.twitter.login" -> "",
-    "test.twitter.password" -> "",
-    "test.twitter.id" -> "",
-    "play2sec.twitter.consumerKey" -> "",
-    "play2sec.twitter.consumerSecret" -> "",
-    "test.google.login" -> "",
-    "test.google.password" -> "",
-    "test.google.id" -> "",
-    "play2sec.google.clientId" -> "",
-    "play2sec.google.clientSecret" -> ""
-  )
+  val config =
+    configByProvider(FacebookAuthProvider.PROVIDER_KEY) ++
+    configByProvider(TwitterAuthProvider.PROVIDER_KEY) ++
+    configByProvider(GoogleAuthProvider.PROVIDER_KEY)
 }
